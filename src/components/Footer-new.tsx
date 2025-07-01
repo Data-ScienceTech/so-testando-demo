4aebaae99d82a38709357e6a8784f2df2dbcbc7d
Carlos Denner <carlosdenner@gmail.com>
2025-07-01 12:18:56 -0400

    chore: apply customization client_phone

--- src/components/Footer-new.tsx
+++ src/components/Footer-new.tsx
@@ -38,7 +38,7 @@ export const Footer = () => {
             <h3 className="font-semibold mb-4">Contact</h3>
             <ul className="space-y-2 text-gray-400">
               <li>cacadfa@fankknnk.com</li>
-              <li>[CLIENT_PHONE]</li>
+              <li>328932893289</li>
             </ul>
           </div>
         </div>
